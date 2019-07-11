44cab7e69e74f5e98d119e62f88d9c934a86660e
LucasCtrl <lucasalt@protonmail.com>
2019-07-11 21:46:51 +0200

    Add some config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,15 @@
 export default {
-    head: {
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-        ]
-    }
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'icon', href: 'favicon.jpg', type: 'image/jpeg' }
+    ]
+  },
+  css: [
+    'minireset.css',
+    '@/assets/scss/app.scss'
+  ]
 }
