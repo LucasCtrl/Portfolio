764f10a4a74b6dd739e0cd0e2eccf9972d75d9a0
LucasCtrl <lucasalt@protonmail.com>
2019-07-11 22:06:30 +0200

    Add page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,19 @@
 export default {
   head: {
+    titleTemplate: (titleChunk) => {
+      return titleChunk ? `${titleChunk} - LucasAlt` : 'LucasAlt'
+    },
+
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
+
     link: [
       { rel: 'icon', href: 'favicon.jpg', type: 'image/jpeg' }
     ]
   },
+
   css: [
     'minireset.css',
     '@/assets/scss/app.scss'
